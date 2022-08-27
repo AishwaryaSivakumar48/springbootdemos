9c455b8248fa6c3c0113726023f9f5a472a93bd9
Aishwarya <aishwarya.sivakumar48@gmail.com>
2022-08-27 12:57:51 +0530

    changes

---
+++ S01springboothelloworld/src/main/java/com/example/demo/S01springboothelloworldApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S01springboothelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S01springboothelloworldApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
